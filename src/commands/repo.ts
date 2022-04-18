a30aeb2bb5355d96a93c4802af77c4266e26b44e
Luke <sabolukas03@gmail.com>
2022-04-18 14:57:30 +0200

    refactor(commands/repo): Throw error when the data fetch failed

--- src/commands/repo.ts
+++ src/commands/repo.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CacheType, CommandInteractionOption, MessageEmbed } from 'discord.js';
+import { CommandInteraction, MessageEmbedOptions } from 'discord.js';
 import { Command } from '../interfaces/command';
 import fetch from 'node-fetch';
 
@@ -13,27 +13,27 @@ const Repo: Command = {
   run: async (interaction) => {
     const { channel } = interaction;
     if (!channel) return;
-    await newEmbed(channel, interaction.options.getString('name', true));
+    return newEmbed(interaction, interaction.options.getString('name', true));
   },
 };
 
-const newEmbed = async (channel: any, repository: string) => {
-  console.log(repository);
+const newEmbed = async (interaction: CommandInteraction, repository: string) => {
   const response = await fetch(`https://api.github.com/repos/overextended/${repository}`);
+  if (response.status !== 200) return interaction.reply('No such Overextended repository found.');
   const data = await response.json();
-  const repoEmbed = new MessageEmbed()
-    .setColor('#2696e0')
-    .setTitle(data.name)
-    .setDescription(data.description)
-    .setThumbnail('https://i.imgur.com/Rp4xZiU.png')
-    .addFields(
+  const repoEmbed: MessageEmbedOptions = {
+    color: '#2696e0',
+    title: data.name,
+    description: data.description,
+    thumbnail: { url: 'https://i.imgur.com/Rp4xZiU.png' },
+    fields: [
       { name: 'Watchers', value: data.subscribers_count.toString(), inline: true },
       { name: 'Forks', value: data.forks.toString(), inline: true },
-      { name: 'Stars', value: data.stargazers_count.toString(), inline: true }
-    )
-    .setURL(`https://github.com/overextended/${repository}`);
-  //   console.log(data);
-  return channel.send({ embeds: [repoEmbed] });
+      { name: 'Stars', value: data.stargazers_count.toString(), inline: true },
+    ],
+    url: `https://github.com/overextended/${repository}`,
+  };
+  return interaction.reply({ embeds: [repoEmbed] });
 };
 
 export default Repo;
