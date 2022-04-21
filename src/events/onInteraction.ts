d8f1ac00df20f2c3a68417ff52a49ad87a153abc
Luke <sabolukas03@gmail.com>
2022-04-21 10:52:34 +0200

    feat(commands): Log mod command uses

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,10 +1,15 @@
-import { Interaction } from 'discord.js';
+import { Interaction, MessageEmbedOptions, TextChannel } from 'discord.js';
 import { CommandList } from '../commands';
+import { log_channel } from '../settings.json';
 
 export const onInteraction = async (interaction: Interaction) => {
   if (!interaction.isCommand()) return;
   for (const Command of CommandList) {
     if (interaction.commandName === Command.data.name) {
+      if (interaction.commandName === 'mod') {
+        const logChannel = interaction.guild?.channels.cache.get(log_channel) as TextChannel;
+        logChannel && logChannel.send(`${interaction.user.tag} used **mod**!`);
+      }
       await Command.run(interaction);
       break;
     }
