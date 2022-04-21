16704a3b5369f203f86d544bf64f15fc4163bf89
Luke <sabolukas03@gmail.com>
2022-04-21 11:33:18 +0200

    feat(commands): Docs command

---
+++ src/commands/docs.ts
@@ -0,0 +1,27 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbedOptions } from 'discord.js';
+import { Command } from '../interfaces/command';
+
+const Docs: Command = {
+  data: new SlashCommandBuilder()
+    .setName('docs')
+    .setDescription('Get a documentation link for an Overextended resource.')
+    .addStringOption((option) => option.setName('resource').setDescription('Resource to get the documentation for.')),
+  run: async (interaction) => {
+    const resource = interaction.options.getString('resource') || '';
+    const docsEmbed: MessageEmbedOptions = {
+      title: 'Documentation',
+      description: 'Please read the documentation thoroughly and carefully.',
+      color: 'GREEN',
+      fields: [
+        {
+          name: resource || 'Website',
+          value: `https://overextended.github.io/docs/${resource}`,
+        },
+      ],
+    };
+    return await interaction.reply({ embeds: [docsEmbed] });
+  },
+};
+
+export default Docs;
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ import { Command } from '../interfaces/command';
 import Mod from './mod';
 import Ping from './ping';
 import Repo from './repo';
+import Docs from './docs';
 
-export const CommandList: Command[] = [Ping, Repo, Mod];
+export const CommandList: Command[] = [Ping, Repo, Mod, Docs];
