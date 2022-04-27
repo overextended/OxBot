85e0fca1680899c4da0c2a8ac3ff2ae27507755a
Luke <sabolukas03@gmail.com>
2022-04-27 21:43:59 +0200

    feat(commands): Add Ox command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,5 +4,6 @@ import Ping from './ping';
 import Repo from './repo';
 import Docs from './docs';
 import Guild from './framework';
+import Ox from './ox';
 
-export const CommandList: Command[] = [Ping, Repo, Mod, Docs, Guild];
+export const CommandList: Command[] = [Ping, Repo, Mod, Docs, Guild, Ox];
---
+++ src/commands/ox.ts
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbedOptions } from 'discord.js';
+import { Command } from '../interfaces/command';
+
+const OxEmbed: MessageEmbedOptions = {
+  title: 'Overextended',
+  color: 'AQUA',
+  description:
+    'Overextended is a small group working to create open-source resoruces for FiveM, with focus on security, performance and stability.',
+  thumbnail: { url: 'https://i.imgur.com/Rp4xZiU.png' },
+  fields: [
+    {
+      name: 'Team',
+      value: 'If you wish to donate to any of the group members, their donation links are listed below.',
+    },
+    {
+      name: 'Linden',
+      value: 'https://ko-fi.com/thelindat',
+    },
+    {
+      name: 'Dunak',
+      value: 'https://ko-fi.com/dunak',
+    },
+    {
+      name: 'Luke',
+      value: 'https://ko-fi.com/lukewastaken',
+    },
+    {
+      name: 'DokaDoka',
+      value: 'https://ko-fi.com/dokadoka',
+    },
+  ],
+};
+
+const Ox: Command = {
+  data: new SlashCommandBuilder().setName('ox').setDescription('About Overextended'),
+  run: async (interaction) => await interaction.reply({ embeds: [OxEmbed] }),
+};
+
+export default Ox;
