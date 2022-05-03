49fad4f1ea44e29338f66355c5b2a164e728e402
Luke <sabolukas03@gmail.com>
2022-05-03 12:51:41 +0200

    fix(events): Use timestamp for log and member for onMemeberRemove

--- src/events/onMemberRemove.ts
+++ src/events/onMemberRemove.ts
@@ -5,7 +5,7 @@ export const onMemberRemove = async (member: GuildMember | PartialGuildMember) =
   const logs = await member.guild.fetchAuditLogs({ limit: 1, type: 'MEMBER_KICK' });
   const log = logs.entries.first();
   if (!log) return console.log('No audit log for kicked member found.');
-  if (member.joinedAt && log.createdAt < member.joinedAt) return;
+  if (member.joinedTimestamp && log.createdTimestamp < member.joinedTimestamp) return;
   if (log.executor?.id === '874059310869655662') return; // Check for Warden
 
   const kickEmbed: MessageEmbedOptions = {
