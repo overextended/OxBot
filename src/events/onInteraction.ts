8d64d2e48a6a6fa6ae940bb5562ba29b39738f4c
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-04-29 14:27:18 +0200

    feat(commands): bulkunban command (#2)
    * feat(commands): bulkunban command
    
    * tweak(commands/bulkunban): use fetch on bans

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,4 +1,4 @@
-import { Interaction, MessageEmbedOptions, TextChannel } from 'discord.js';
+import { Interaction, TextChannel } from 'discord.js';
 import { CommandList } from '../commands';
 import { log_channel } from '../settings.json';
 
@@ -6,9 +6,9 @@ export const onInteraction = async (interaction: Interaction) => {
   if (!interaction.isCommand()) return;
   for (const Command of CommandList) {
     if (interaction.commandName === Command.data.name) {
-      if (interaction.commandName === 'mod') {
+      if (interaction.commandName === 'mod' || interaction.commandName === 'bulkunban') {
         const logChannel = interaction.guild?.channels.cache.get(log_channel) as TextChannel;
-        logChannel && logChannel.send(`${interaction.user.tag} used **mod**!`);
+        logChannel && logChannel.send(`${interaction.user.tag} used **${interaction.commandName}**!`);
       }
       await Command.run(interaction);
       break;
