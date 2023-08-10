44b046a9e6ce60d575c492739406d3e3c3914bb7
Judd <85649796+juddisjudd@users.noreply.github.com>
2023-08-10 10:44:47 -0700

    tweak(events/onMessageDelete): misc changes (#5)
    Error Handling, hard limit thingies, partialmessage check, foreach instead of map since we are not transforming data.

--- src/events/onMessageDelete.ts
+++ src/events/onMessageDelete.ts
@@ -2,26 +2,51 @@ import { Message, MessageEmbedOptions, PartialMessage, TextChannel } from 'disco
 import { Bot } from '..';
 import { log_channel } from '../settings.json';
 
-export const onMessageDelete = async (message: Message<boolean> | PartialMessage) => {
-  const fields = [{ name: 'Message', value: message.content || '**NO MESSAGE SENT**' }];
-  message.attachments.map((attachement) =>
-    fields.push({ name: attachement.name || '**NO ATTACHEMENT NAME FOUND**', value: attachement.attachment as string })
-  );
+export const onMessageDelete = async (message: Message | PartialMessage) => {
+    if (message.partial) {
+        try {
+            await message.fetch();
+        } catch (error) {
+            console.error("Error fetching the message:", error);
+            return;
+        }
+    }
 
-  const messageDeleteEmbed: MessageEmbedOptions = {
-    title: 'Message removed',
-    description: `<@${message.author?.id}> removed a message in <#${message.channel.id}>`,
-    author: {
-      name: message.author?.username,
-      iconURL: message.author?.displayAvatarURL(),
-    },
-    color: 'RED',
-    fields,
-  };
+    const fields = [{ name: 'Message', value: message.content || '**NO MESSAGE SENT**' }];
 
-  const logChannel = (await Bot.channels.fetch(log_channel)) as TextChannel;
+    message.attachments.forEach((attachment, index) => {
+        if (index < 24) { 
+            fields.push({ 
+                name: attachment.name || '**NO ATTACHEMENT NAME FOUND**', 
+                value: attachment.attachment as string 
+            });
+        }
+    });
 
-  if (!logChannel) return;
+    const messageDeleteEmbed: MessageEmbedOptions = {
+        title: 'Message removed',
+        description: `<@${message.author?.id}> removed a message in <#${message.channel.id}>`,
+        author: {
+            name: message.author?.username,
+            iconURL: message.author?.displayAvatarURL(),
+        },
+        color: 'RED',
+        fields,
+    };
 
-  await logChannel.send({ embeds: [messageDeleteEmbed] });
+    let logChannel: TextChannel;
+    try {
+        logChannel = (await Bot.channels.fetch(log_channel)) as TextChannel;
+    } catch (error) {
+        console.error("Error fetching the log channel:", error);
+        return;
+    }
+
+    if (!logChannel) return;
+
+    try {
+        await logChannel.send({ embeds: [messageDeleteEmbed] });
+    } catch (error) {
+        console.error("Error sending the embed to the log channel:", error);
+    }
 };
