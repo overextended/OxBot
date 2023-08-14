5117d5c44989206483bb79290f9d963aa95ce0c3
Judd <85649796+juddisjudd@users.noreply.github.com>
2023-08-13 23:59:47 -0700

    Update onMessageDelete.ts
    -Tested with regular messages and image/gif messages.
    -Added Author ID, Message ID and timestamp to footer.
    -Slight change to wording.
    Preview changes here:
    https://i.imgur.com/Yf2X6Zy.png

--- src/events/onMessageDelete.ts
+++ src/events/onMessageDelete.ts
@@ -1,54 +1,44 @@
-import { Message, MessageEmbedOptions, PartialMessage, TextChannel } from 'discord.js';
+import { Message, MessageEmbed, PartialMessage, TextChannel } from 'discord.js';
 import { Bot } from '..';
 import { log_channel } from '../settings.json';
 
 export const onMessageDelete = async (message: Message | PartialMessage) => {
-  if (message.partial) {
-    try {
-      await message.fetch();
-    } catch (error) {
-      console.error('Error fetching the message:', error);
-      return;
+    if (message.partial) {
+        try {
+            await message.fetch();
+        } catch (error) {
+            console.error('Error fetching the message:', error);
+            return;
+        }
     }
-  }
 
-  const fields = [{ name: 'Message', value: message.content || '**NO MESSAGE SENT**' }];
-
-  let i = 0;
-  message.attachments.forEach((attachment) => {
-    if (i < 24) {
-      fields.push({
-        name: attachment.name || '**NO ATTACHEMENT NAME FOUND**',
-        value: attachment.attachment as string,
-      });
+    const authorTag = message.author?.tag || 'Unknown Author';
+    const content = message.content || '**NO MESSAGE SENT**';
+    const authorId = message.author?.id || 'Unknown ID';
+    const messageId = message.id || 'Unknown ID';
+    
+    const embed = new MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle('Message Deleted')
+        .setDescription(`Message sent by <@${authorId}> in ${message.channel.toString()} was deleted.`)
+        .addField('Message Content', `${content}\n\n`)
+        .setFooter({ text: `Author ID: ${authorId} | Message ID: ${messageId}` })
+        .setTimestamp(message.createdTimestamp)
+        .setAuthor({
+            name: message.author?.username || 'Unknown Username',
+            iconURL: message.author?.displayAvatarURL() || undefined
+        });
+    
+    let i = 0;
+    message.attachments.forEach((attachment) => {
+        if (i < 24) {
+            embed.addField(`Attachment ${i+1}`, attachment.url);
+            i++;
+        }
+    });
+    
+    const logChannel = Bot.channels.cache.get(log_channel) as TextChannel;
+    if (logChannel) {
+        logChannel.send({ embeds: [embed] });
     }
-    i++;
-  });
-
-  const messageDeleteEmbed: MessageEmbedOptions = {
-    title: 'Message removed',
-    description: `Message sent by <@${message.author?.id}> was removed in <#${message.channel.id}>`,
-    author: {
-      name: message.author?.username,
-      iconURL: message.author?.displayAvatarURL(),
-    },
-    color: 'RED',
-    fields,
-  };
-
-  let logChannel: TextChannel;
-  try {
-    logChannel = (await Bot.channels.fetch(log_channel)) as TextChannel;
-  } catch (error) {
-    console.error('Error fetching the log channel:', error);
-    return;
-  }
-
-  if (!logChannel) return;
-
-  try {
-    await logChannel.send({ embeds: [messageDeleteEmbed] });
-  } catch (error) {
-    console.error('Error sending the embed to the log channel:', error);
-  }
 };
