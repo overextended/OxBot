feb0cce17fc961b7aff52c90173f894adeee5141
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2023-08-29 13:48:59 -0700

    refactor: Update command handling

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,11 +1,11 @@
 import { Client, TextChannel, REST, Routes } from 'discord.js';
-import { CommandList } from '../commands';
+import commands from '../handlers/commandHandler';
 import Config from '../config';
 import { log_channel } from '../settings.json';
 
 export const onReady = async (Bot: Client) => {
   const rest = new REST({ version: '9' }).setToken(Config.DISCORD_TOKEN);
-  const commandData = CommandList.map((command) => command.data.toJSON());
+  const commandData = Array.from(commands.values()).map((command) => command.data.toJSON());
   await rest.put(Routes.applicationGuildCommands(Config.CLIENT_ID, Config.GUILD_ID), { body: commandData });
   const logChannel = Bot.channels.cache.get(log_channel) as TextChannel;
   logChannel && (await logChannel.send('Bot started.'));
