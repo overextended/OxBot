a8d61f40bf094e3d27fd82387d4894c49421dab8
Judd <85649796+juddisjudd@users.noreply.github.com>
2023-08-30 13:25:25 -0700

    refactor: update command handling (#14)

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,22 +1,24 @@
 import { Interaction, TextChannel } from 'discord.js';
-import { CommandList } from '../commands';
+import commands from '../handlers/commandHandler';
 import { log_channel } from '../settings.json';
 
 export const onInteraction = async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  for (const Command of CommandList) {
-    if (interaction.commandName === Command.data.name) {
-      if (interaction.commandName === 'mod' || interaction.commandName === 'bulkunban') {
-        const logChannel = interaction.guild?.channels.cache.get(log_channel) as TextChannel;
-        logChannel && logChannel.send(`${interaction.user.tag} used **${interaction.commandName}**!`);
-      }
+  const command = commands.get(interaction.commandName);
+  if (!command) return;
 
-      if (typeof Command.run === 'function') {
-        await Command.run(interaction);
-      }
+  if (interaction.commandName === 'mod' || interaction.commandName === 'bulkunban') {
+    const logChannel = interaction.guild?.channels.cache.get(log_channel) as TextChannel;
+    logChannel && logChannel.send(`${interaction.user.tag} used **${interaction.commandName}**!`);
+  }
 
-      break;
+  if (typeof command.run === 'function') {
+    try {
+      await command.run(interaction);
+    } catch (error) {
+      console.error(`Error executing command ${interaction.commandName}:`, error);
+      await interaction.reply({ content: 'There was an error executing that command.', ephemeral: true });
     }
   }
 };
