13b89256907f17b04abdb7d6f00ca7a8f14722a6
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-01-04 00:09:04 -0800

    refactor/update: Update Dockerfile & Regex / Moved things out of onMessageCreate

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -1,37 +1,6 @@
 import { Message } from 'discord.js';
-
-const ignoredRoles = [
-  '814181424840179733', // Admin
-  '819891382114189332', // Shrimp Supreme
-  '933681479878324234', // Dictator
-  '892853647950618675', // Overextended
-  '831961060314972170', // Senior Moderator
-  '945991885783175189', // Moderator
-  '906292806098759750', // Contributor
-  '1120932120056057926', // Affiliate
-  '816709868764921876', // Coffee Drinker
-  '842456416019021895', // Recognized Member
-  '1140367518230397029', // GitHub
-];
-
-const patterns = [
-  /(\bhelp\b|\bhlep\b|\bhalp\b)/i, // help, hlep, halp
-  /(\bsupport\b|\bsuport\b)/i, // support, suport
-  /(\bhow\s+can\b)/i, // how can
-  /(\bcant\b|\bcan't\b)/i, // cant, can't
-  /(\btype\b)/i, // type
-  /(\bpost\b)/i, // post
-  /(\bhow\b)/i, // how
-  /(\bsomehow\b)/i, // somehow
-  /(\bis\s+there\b)/i, // is there
-  /(\bcan\b)/i, // can
-  /(\bpossible\b)/i, // possible
-  /(\berror\b)/i, // error
-  /(\bwont\b)/i, // wont
-  /(\bconnect\b)/i, // connect
-];
-
-const guidelines = '<#1114827068337815612>';
+import { ignoredRoles, whitelistedChannels, guidelines } from '../constants';
+import { patterns } from '../utils/patterns';
 
 interface ChannelResponses {
   [key: string]: string[];
@@ -51,18 +20,12 @@ const channelSpecificResponses: ChannelResponses = {
   ],
 };
 
-const whitelistedChannelIds = [
-  '829915571394052176', // coffee-club
-  '906294817678590012', // nerd-club
-  `844240881283366962`, // shitposting
-];
-
 export const onMessageCreate = async (message: Message) => {
   if (message.author.bot) {
     return;
   }
 
-  if (whitelistedChannelIds.includes(message.channelId)) {
+  if (whitelistedChannels.includes(message.channelId)) {
     return;
   }
 
