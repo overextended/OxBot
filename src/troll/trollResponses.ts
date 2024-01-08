580bbae7f39eccacf41d85be1cc47f47c371d6c6
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-01-08 11:11:24 -0800

    update: troll responses

--- src/troll/trollResponses.ts
+++ src/troll/trollResponses.ts
@@ -4,26 +4,26 @@ export const customTrollResponses = new Map<string, string[]>();
 customTrollResponses.set('300457650615746561', [
   "Mate, you're about as useful as a chocolate teapot in the Outback.",
   "You're as reliable as a waterproof teabag, mate.",
-  'You’re like a painting by a blindfolded gorilla.',
-  'You smell like a wet koala, mate.',
-  'Hey.',
+  "You're like a painting by a blindfolded gorilla.",
+  "You smell like a wet koala, mate.",
+  "Hey.",
   "Can you let your mum know that I'm not coming over tonight?",
-  'If you wanted me to touch you, you could have just asked.',
+  "If you wanted me to touch you, you could have just asked.",
   "pssst...I know your secrets.",
 ]);
 
 // Andy
 customTrollResponses.set('243440367066677251', [
-  'psst...No one uses NDCore.',
-  'ND = Andy? So is it actually AndyCore? Who is names a framework after themselves?',
-  'NDCore is a joke.',
-  'What even is NDCore?',
+  "psst...No one uses NDCore.",
+  "ND = Andy? So is it actually AndyCore? Who is names a framework after themselves?",
+  "NDCore is a joke.",
+  "What even is NDCore?",
 ]);
 
 // Scully
 customTrollResponses.set('530698981920079882', [
-  'psst...No one uses NDCore.',
-  'ND = Andy? So is it actually AndyCore? Who is names a framework after themselves?',
-  'NDCore is a joke.',
-  'What even is NDCore?',
+  "Shut up already.",
+  "You like to talk a lot, don't you?",
+  "Are you seriously selling an Umbrella Glider for $5? Alright Mary Poppins.",
+  "Are you seriously selling a Bodycam UI for $5? It's not even original.",
 ]);
