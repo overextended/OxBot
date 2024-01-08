e5210b947dbeaf982867de3dea38d40c2d3750d1
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-01-08 14:46:02 -0800

    add: more positive patterns

--- src/utils/patterns.ts
+++ src/utils/patterns.ts
@@ -80,6 +80,14 @@ export const positivePatterns = [
   /(\bno\s+work\b)/i, // no work
   /(\bdoesn'?t\s+work\b)/i, // doesn't work, doesnt work
   /(\bdoes\s+anyone\s+know\b)/i, // does anyone know
+  /(\bhow\s+do\s+i\b)/i, // how do i
+  /(\bhow\s+to\s+fix\b)/i, // How to fix
+  /(\bneed\s+advice\s+on\b)/i, // Need advice on
+  /(\bwhat\s+should\s+I\s+do\s+about\b)/i, // What should I do about
+  /(\bdon't\s+understand\s+how\s+to\b)/i, // Don't understand how to
+  /(\bconfused\s+about\b)/i, // Confused about
+  /(\blooking\s+for\s+information\s+on\b)/i, // Looking for information on
+  /(\bseeking\s+help\s+with\b)/i, // Seeking help with
 ];
 
 export const negativePatterns = [
