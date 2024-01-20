4fb1ab051286d7eb60b09928af4718237a618de8
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-01-20 01:05:20 -0800

    refactor: optimize lookuip/cleaner main func logic/error handling

--- src/events/onSlashCommand.ts
+++ src/events/onSlashCommand.ts
@@ -1,32 +1,34 @@
 import { Interaction, EmbedBuilder, TextChannel } from 'discord.js';
 import { log_channel } from '../settings.json';
 
-const commandWhitelist = ['docs', 'issue', 'ox', 'ping', 'repo', 'guild', 'whois'];
+const commandWhitelist = new Set(['docs', 'issue', 'ox', 'ping', 'repo', 'guild', 'whois']);
+
+function getChannelMention(channel: TextChannel | null) {
+  return channel ? `<#${channel.id}>` : 'Unknown Channel';
+}
 
 export async function onSlashCommand(interaction: Interaction) {
-  if (!interaction.isCommand()) return;
+  if (!interaction.isCommand() || !interaction.guild) return;
 
-  if (commandWhitelist.includes(interaction.commandName)) {
-    return;
-  }
+  if (!commandWhitelist.has(interaction.commandName)) {
+    const channelMention = getChannelMention(interaction.channel as TextChannel);
 
-  const channelName = interaction.channel?.toString() || 'Unknown Channel';
+    try {
+      const logEmbed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Slash Command Used')
+        .setDescription(`User <@${interaction.user.id}> used \`/${interaction.commandName}\` in ${channelMention}.`)
+        .setTimestamp()
+        .setFooter({ text: `User ID: ${interaction.user.id}` });
 
-  try {
-    const logEmbed = new EmbedBuilder()
-      .setColor('#0099ff')
-      .setTitle('Slash Command Used')
-      .setDescription(`User <@${interaction.user.id}> used \`/${interaction.commandName}\` in ${channelName}.`)
-      .setTimestamp(new Date())
-      .setFooter({ text: `User ID: ${interaction.user.id}` });
-
-    const logChannel = interaction.guild?.channels.cache.get(log_channel) as TextChannel;
-    if (logChannel) {
-      await logChannel.send({ embeds: [logEmbed] });
-    } else {
-      console.error(`Log channel with ID ${log_channel} not found`);
+      const logChannel = (await interaction.guild.channels.fetch(log_channel).catch(console.error)) as TextChannel;
+      if (logChannel) {
+        await logChannel.send({ embeds: [logEmbed] });
+      } else {
+        console.error(`Log channel with ID ${log_channel} not found`);
+      }
+    } catch (error) {
+      console.error('Failed to log slash command usage:', error);
     }
-  } catch (error) {
-    console.error('Failed to log slash command usage:', error);
   }
 }
