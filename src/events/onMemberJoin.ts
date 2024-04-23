631cc27e28e288c221da2b3a2474fa38ca0e589d
Judd <85649796+juddisjudd@users.noreply.github.com>
2024-04-23 15:10:12 -0700

    Fix DM Error and Update Warn Command; Automate Member Role Assignment on Server Join (#45)
    * fix/update: error handling in warn command / Auto Member Role
    
    * Update warning DM error message
    
    * Update warning message to include reason in warn command

--- src/events/onMemberJoin.ts
+++ src/events/onMemberJoin.ts
@@ -7,4 +7,19 @@ export const onMemberJoin = async (member: GuildMember) => {
 
   const welcomeMessage = `Welcome <@${member.id}> to the server!`;
   channel.send(welcomeMessage);
+
+  const roleId = '842455998266605610'; // Overextended Member RoleID
+  const role = member.guild.roles.cache.get(roleId);
+
+  if (!role) {
+    console.error(`Role not found: ${roleId}`);
+    return;
+  }
+
+  try {
+    await member.roles.add(role);
+    console.log(`Assigned role '${role.name}' to ${member.displayName}`);
+  } catch (error) {
+    console.error(`Could not assign role to ${member.displayName}: ${error}`);
+  }
 };
